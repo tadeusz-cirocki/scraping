01a57b00df3ca1f24a6112254bc392d437d1a3d9
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2023-07-10 02:22:44 +0200

    deduplicate movies

--- scraper.js
+++ scraper.js
@@ -1,7 +1,34 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-//todo top 10 best rated movies from top 4 streaming platforms from current year
+const currentYear = new Date().getFullYear();
+
+// Function to scrape movies from filmweb from a a given platform
+const scrapeMovies = async (platform) => {
+    try {
+        const url = `https://www.filmweb.pl/ranking/vod/${platform}/film/${currentYear}`
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        const movies = [];
+
+        $('.rankingType.rankingType').each((index, element) => {
+            if (index < 10) {
+                const title = $(element).find('.rankingType__title').text().trim();
+                const rating = $(element).find('.rankingType__rate--value').text().trim().replace(',', '.');
+
+                movies.push({ title, platform, rating });
+            }
+        });
+
+        return movies;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+};
 
 /*
 I've decided to hardcode names for the top 4 platforms because these are not changing often or at all. 
@@ -14,48 +41,43 @@ e.g prime video is "amazon" in the filmweb URL
 */
 const platforms = ["netflix", "hbo_max", "canal_plus_manual", "disney"];
 
-const currentYear = new Date().getFullYear();
-
-// Function to scrape movies from a given URL
-const scrapeMovies = async (url, platform) => {
-    try {
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
-
-        const movies = [];
-
-        $('.rankingType.hasVod').each((index, element) => {
-            if (index < 10) {
-                const title = $(element).find('.rankingType__title').text().trim();
-                const vodService = platform;
-                const rating = $(element).find('.rankingType__rate--value').text().trim().replace(',', '.');
-
-                movies.push({ title, vodService, rating });
-            }
-        });
-
-        return movies;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return [];
-    }
-};
-
-// Scrape movies from all URLs
+// Scrape movies from all platforms
 const scrapeAllMovies = async () => {
     const allMovies = [];
 
-    for (let i = 0; i < platforms.length; i++) {
-        const platform = platforms[i];
-        const url = `https://www.filmweb.pl/ranking/vod/${platform}/film/${currentYear}`;
+    // Create an array of promises for scraping movies
+    const promises = platforms.map(platform => {
+        return scrapeMovies(platform);
+    });
 
-        const movies = await scrapeMovies(url, platform);
+    // Execute all promises concurrently using Promise.all
+    const results = await Promise.all(promises);
+
+    // Combine the results into a single array
+    results.forEach(movies => {
         allMovies.push(...movies);
-    }
+    });
 
-    return allMovies;
+    // Deduplicate movies based on title and keep highest rating
+    const deduplicatedMovies = {};
+    allMovies.forEach(movie => {
+        const { title, platform, rating } = movie;
+        if (deduplicatedMovies[title]) {
+            if (rating > deduplicatedMovies[title].rating) {
+                deduplicatedMovies[title].rating = rating;
+                deduplicatedMovies[title].platform = platform;
+            }
+        } else {
+            deduplicatedMovies[title] = { title, platform, rating };
+        }
+    });
+
+    // Convert deduplicated movies object to array and sort by rating in descending order
+    const sortedMovies = Object.values(deduplicatedMovies).sort((a, b) => b.rating - a.rating);
+
+    return sortedMovies;
 };
+
 
 // Run the scraping function
 scrapeAllMovies()
